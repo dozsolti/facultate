e60f11902d4e5f1dfc2451e7a81dcb6527bf57f8
DESKTOP-S4B16IQ\Zsolti <d.zsolti@yahoo.com>
2018-03-08 14:57:18 +0200

    Matrix
    Rotirea si oglindirea unei matrici

---
+++ Visual Studio Projects/FlippingMatrix/FlippingMatrix/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlippingMatrix
+{
+    class Program
+    {
+        static Random rnd;
+        static int[,] m;
+        static int n;
+        static void Main(string[] args)
+        {
+            rnd = new Random();
+            n = 5;
+            m = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    m[i, j] = rnd.Next(10);
+                }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(m[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+            /*
+            Console.WriteLine();
+            Console.WriteLine("FlipY:  ");
+            FlipY();
+            AfiseazaM();
+            }*/
+
+            /*Console.WriteLine();
+            Console.WriteLine("FlipX:  ");
+            FlipX();
+            AfiseazaM();*/
+
+            /*Console.WriteLine();
+            Console.WriteLine("FlipDp:  ");
+            FlipDP();*/
+            Console.WriteLine();
+
+            //Console.WriteLine("FlipDS:  ");
+            //FlipDS();
+
+            Console.WriteLine("Flip la Stanga");
+            Rotate("stanga");
+            
+            Console.WriteLine("Flip la Dreapta");
+            Rotate("dreapta");
+            AfiseazaM();
+
+            Console.ReadKey();
+        }
+
+        private static void Rotate(string dir)
+        {
+            if (dir == "stanga")
+            {
+                int[,] b = new int[n, n];
+                for (int i = 0; i <n; i++)
+                    for (int j = 0; j <n; j++)
+                    {
+                        b[n - 1 - j, i] = m[i, j];
+                    }
+                m = b;
+            }
+            if (dir == "dreapta")
+            {
+                int[,] b = new int[n, n];
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        b[i,j] = m[n - 1 - j, i];
+                    }
+                m = b;
+            }
+        }
+
+        private static void AfiseazaM()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(m[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void FlipDS()
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (i + j < n - 1)
+                    {
+                        int t = n - 1 - (i + j);
+                        Swap(ref m[i, j], ref m[i+t, j+t]);
+                    }
+                }
+        }
+        private static void FlipDP()
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if(i<j)
+                        Swap(ref m[i, j], ref m[j, i]);
+                }
+        }
+
+        private static void FlipX()
+        {
+            for (int i = 0; i < n/2; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    Swap(ref m[n - 1 - i, j], ref m[i, j]);
+                }
+        }
+
+        static void FlipY()
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n/2; j++)
+                {
+                    Swap(ref m[i,n-1-j],ref m[i,j]);
+                }
+        }
+
+        private static void Swap(ref int v1,ref int v2)
+        {
+            int a=v1;
+            v1 = v2;
+            v2 = a;
+        }
+    }
+}
---
+++ Visual Studio Projects/Matrici2.0/Matrici2.0/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrici2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[5,5];
+            int n = 4;
+            int k = 0;
+            int nrIt = 0;
+            for (int i = 0; i <= 2 * n - 1; i++)
+            {
+                nrIt++;
+                if (i <= n)
+                {
+                    // /
+                    for (int j = 0; j < i; j++)
+                    {
+                        nrIt++;
+                        k++;
+                        a[j,i-1-j] = k;
+            
+                    }
+
+                }
+                else
+                {
+                    // /
+                    int _i = i % n;
+                    Console.WriteLine("_: " + (n+1-_i));
+                    for (int j = 0; j < (n+1-_i); j++)
+                    {
+                        nrIt++;
+                        k++;
+                        a[_i, n-_i] = k;
+                        Console.WriteLine("i: "+_i+" j: "+j);
+
+                    }
+                    
+                }
+
+
+            }
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++)
+                    Console.Write(a[i,j]+" ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("nrIt: "+nrIt);
+            Console.ReadKey();
+
+
+        }
+    }
+}
