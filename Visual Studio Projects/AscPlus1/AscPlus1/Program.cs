6fbe334d83a91f16a7a3a31dcd86508ab90fd8aa
DESKTOP-S4B16IQ\Zsolti <d.zsolti@yahoo.com>
2018-03-09 00:20:39 +0200

    mutarea variabilelor
    am declarat variabilele la inceput pentru a nu declara de fiecare data

--- Visual Studio Projects/AscPlus1/AscPlus1/Program.cs
+++ Visual Studio Projects/AscPlus1/AscPlus1/Program.cs
@@ -10,23 +10,27 @@ namespace AscPlus1
     {
         static int[] numerelePosibile =  { 1, 2 };
         static int n ;
+        static ulong nFromArray;
         static int[] x;
         static ulong nrafis;
         static int nrMaxDeDoi;
         static int startCount;
 
+        static int maxLen;
+        static int iForNumber;
+        static ulong _temp;
         static void Main(string[] args)
         {
 
             Console.WriteLine("             Loading please wait...");
-            Console.WriteLine("----------------------------------");
+            Console.WriteLine("------------| "+(System.DateTime.Now.ToString())+" |------------");
 
             // 1820
             // n%2 ==0 ; n /=2
             // else    ; n = 3*n+1
-
+            maxLen = 0;
             nrafis = 0;
-            nrMaxDeDoi = 53;
+            nrMaxDeDoi = 10;
 
             startCount = 4;
 
@@ -35,7 +39,7 @@ namespace AscPlus1
             for (int i = 0; i < startCount; i++)
                 x[i] = 2;
             back2(startCount);
-            //afis();
+            
             Console.WriteLine("\n\n\n----------------------------------");
             Console.WriteLine("             Done.");
             Console.ReadKey();
@@ -43,17 +47,24 @@ namespace AscPlus1
         }
         static void back2(int k)
         {
-            foreach(int i in numerelePosibile)
+            foreach (int i in numerelePosibile)
             {
-                x[k] = i;
-                if (k >= startCount) if (potAdauga(k))
-                        if (k == n - 1) {
-                            afis();
-                            //int q = 2;
-                        }
-                        else back2(k + 1);
-                    else;
-                else back2(k + 1);
+
+                    x[k] = i;
+
+                if (k >= startCount)
+                {
+                    if (eBun(k))
+                    {
+                        
+                            if (k == n - 1)
+                                afis();
+                            else
+                                back2(k + 1);
+                    }
+                }
+                else
+                    back2(k + 1);
             }
         }
 /*        static void back(int k)
@@ -85,11 +96,8 @@ namespace AscPlus1
         {
             for (int i = 0; i < x.Length; i++)
             {
-                if (i == startCount)
-                    Console.Write(".");
                 Console.Write(x[i]);
             }
-            Console.WriteLine();
         }
 
         private static int getComplementara(int i)
@@ -99,13 +107,12 @@ namespace AscPlus1
             return 3 - i;
         }
 
-        static bool potAdauga(int k)
+        static bool eBun(int k)
         {
-            if (x[k] * x[k-1] == 1)
-            {
+            if (x[k] * x[k - 1] == 1) //doi de 1 sa nu fie unul langa altul
                 return false;
-            }
-            if (k > nrMaxDeDoi) {
+            if (k > nrMaxDeDoi) //numarul maxim de 2 de la inceput
+            {
                 bool areDoarDoi = true;
                 for (int i = 0; i <= nrMaxDeDoi; i++)
                 {
@@ -119,30 +126,34 @@ namespace AscPlus1
                     return false;
             }
 
+            
+            /*if ( (k>4) && (x[k] * x[k - 1] * x[k - 2] == 2*2*2)) //doi de 1 sa nu fie unul langa altul
+                return false;
+                */
             return true;
         }
 
         static void afis()
         {
            nrafis++;
-
-           if (nrafis % 16000000 == 0)
+            nFromArray = arrayToNumber(x);
+            int len = nFromArray.ToString().Length;
+            if (nrafis % 16000000 == 0)
             {
-                Console.WriteLine(nrafis);
+                if(maxLen< len)
+                    maxLen = len;
+                Console.WriteLine(nFromArray);
             }
            {
-                ulong n = arrayToNumber(x);
-                if (n.ToString().Length == 15)
+                if (len == 15)
                 {
                     //    Console.Clear();
                     Console.Write(nrafis + ".) ");
 
-                /*for (int i = 0; i < x.Length; i++) {
-                    Console.Write(x[i]);
-                }*/
+                    printArray();
                 
-                    Console.WriteLine(n);
-                 //   Console.WriteLine();
+                    Console.WriteLine("Aaaaaaaaaammmmmmmmmmmmm gasssssssssssssittttttttttttttttt numaaaaaaarrruuuuuuuuuuuuuuuullllll!!!! :"+ nFromArray);
+                    //Console.WriteLine(nFromArray);
                     Console.WriteLine();
                 }
             }
@@ -150,20 +161,20 @@ namespace AscPlus1
 
         private static ulong arrayToNumber(int[] array)
         {
-            int i = 0;
-            ulong n = 1;
-            while( i < array.Length)
+            iForNumber = 0;
+            _temp = 1;
+            while(iForNumber < array.Length)
             {
-                if (array[i] == 1)
-                    if ((n - 1) % 3 == 0)
-                        n = (n - 1) / 3;
+                if (array[iForNumber] == 1)
+                    if ((_temp - 1) % 3 == 0)
+                        _temp = (_temp - 1) / 3;
                     else
                         return 0;
                 else
-                    n = n * 2;
-                i++;
+                    _temp = _temp * 2;
+                iForNumber++;
             }
-            return n;
+            return _temp;
         }
     }
 }
